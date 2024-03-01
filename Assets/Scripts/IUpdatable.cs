48ed9892501fa174ac980ea3b2c3f154df9dd71d
Mikael Dallaire Cote <110583667+0mdc@users.noreply.github.com>
2024-03-01 15:58:05 -0500

    Add IUpdatable doctring.

--- Assets/Scripts/IUpdatable.cs
+++ Assets/Scripts/IUpdatable.cs
@@ -5,6 +5,7 @@ public interface IUpdatable
 {
     /// <summary>
     /// Update the component.
+    /// Called every frame from a MonoBehaviour Update().
     /// </summary>
     public abstract void Update();
 }
