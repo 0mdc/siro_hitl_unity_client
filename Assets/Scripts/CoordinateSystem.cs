d07e4e6f195bd03a7d0055ee536a9076307a7bf8
Mikaël Dallaire Côté <110583667+0mdc@users.noreply.github.com>
2024-03-08 18:30:26 -0500

    Coordinate system fixes and decouple asset loading from `GfxReplayPlayer` (#9)
    * Simplify coordinate system handling. Move instance loading to its own class.
    
    * Rename 'HabitatInstance' to 'GfxReplayInstance'.
    
    * Move ComputeFrameRotationOffset to CoordinateSystem and add test.
    
    * Add comments.
    
    * Add comments.

--- Assets/Scripts/CoordinateSystem.cs
+++ Assets/Scripts/CoordinateSystem.cs
@@ -1,20 +1,15 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 /// <summary>
 /// Collection of utilities to handle coordinate system transformations between Habitat and Unity.
 /// 
 /// Habitat: X-Left, Y-Up, Z-Back
 /// Unity:   X-Left, Y-Up, Z-Forward
-/// 
-/// Beware that because of the change in handedness, the Unity asset pipeline bakes the z-axis flip into 3D models.
-/// See ToUnityQuaternion3DModel() and ToHabitatQuaternion3DModel().
 /// </summary>
 public static class CoordinateSystem
 {
-    private static readonly Quaternion _3dModelRotationCorrection = Quaternion.Euler(0, 180, 0);
-    private static readonly Quaternion _inv3dModelRotationCorrection = Quaternion.Inverse(_3dModelRotationCorrection);
-
     /// <summary>
     /// Convert a Habitat vector into Unity's coordinate system.
     /// </summary>
@@ -41,7 +36,6 @@ public static class CoordinateSystem
     
     /// <summary>
     /// Convert a Habitat quaternion into Unity's coordinate system.
-    /// Beware: the Unity asset pipeline bakes transforms into 3D models. Use ToUnityQuaternion3DModel() to rotate models.
     /// </summary>
     public static Quaternion ToUnityQuaternion(IList<float> rotation)
     {
@@ -49,25 +43,8 @@ public static class CoordinateSystem
             rotation[1],
             rotation[2],
             -rotation[3],
-            -rotation[0]
+            -rotation[0]  // Change of chirality.
         );
-    }
-
-    /// <summary>
-    /// Convert a Habitat quaternion into Unity's coordinate system, taking into account 3D model baked transformations.
-    /// 3D models are pre-processed to handle the change in handedness (avoids negative scaling on the z-axis).
-    /// </summary>
-    public static Quaternion ToUnityQuaternion3DModel(IList<float> rotation)
-    {
-        Quaternion newRot = new Quaternion(
-            rotation[1],
-            -rotation[2],
-            -rotation[3],
-            rotation[0]
-        );
-
-        newRot = _3dModelRotationCorrection * newRot;
-        return newRot;
     }
 
     /// <summary>
@@ -85,35 +62,51 @@ public static class CoordinateSystem
 
     /// <summary>
     /// Convert a Unity quaternion into Habitat's coordinate system.
-    /// Beware: the Unity asset pipeline bakes transforms into 3D models. Use ToHabitatQuaternion3DModel() to rotate models.
     /// </summary>
     public static List<float> ToHabitatQuaternion(Quaternion rotation)
     {
-        Quaternion convertedRotation = _inv3dModelRotationCorrection * rotation;
-
         return new List<float>
         {
-            -convertedRotation.w,
-            convertedRotation.x,
-            convertedRotation.y,
-            -convertedRotation.z
+            -rotation.w,  // Change of chirality.
+            rotation.x,
+            rotation.y,
+            -rotation.z
         };
     }
 
     /// <summary>
-    /// Convert a Unity quaternion into Habitat's coordinate system, taking into account 3D model baked transformations.
-    /// 3D models are pre-processed to handle the change in handedness (avoids negative scaling on the z-axis).
+    /// Create a Unity quaternion from a gfx-replay 'Frame' object.
     /// </summary>
-    public static List<float> ToHabitatQuaternion3DModel(Quaternion rotation)
+    public static Quaternion ComputeFrameRotationOffset(Frame frame)
     {
-        Quaternion convertedRotation = _inv3dModelRotationCorrection * rotation;
+        Quaternion unityFrameQuatInv = Quaternion.Inverse(Quaternion.LookRotation(
+            Vector3.forward,
+            Vector3.up
+        ));
+        Quaternion habitatFrameQuat = Quaternion.LookRotation(
+            frame.front.ToVector3(),
+            frame.up.ToVector3()
+        );
+        return unityFrameQuatInv * habitatFrameQuat;
+    }
+}
 
-        return new List<float>
-        {
-            convertedRotation.w,
-            convertedRotation.x,
-            -convertedRotation.y,
-            -convertedRotation.z
-        };
+public static class Extensions
+{
+    public static Vector3 ToVector3(this IList<float> v)
+    {
+        Assert.IsTrue(v?.Count == 3);
+        return new Vector3(v[0], v[1], v[2]);
+    }
+
+    public static Quaternion ToQuaternion(this IList<float> q)
+    {
+        Assert.IsTrue(q?.Count == 4);
+        return new Quaternion(q[0], q[1], q[2], q[3]);
+    }
+
+    public static float[] ToArray(this Vector3 v)
+    {
+        return new float[]{v.x, v.y, v.z};
     }
 }
---
+++ Assets/Scripts/GfxReplayInstance.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Represents a Habitat object instance from gfx-replay.
+/// Acts as a placeholder when the object is loading or has failed to load.
+/// </summary>
+public class GfxReplayInstance : MonoBehaviour
+{
+    /// <summary>
+    /// Instantiates a 'GfxReplayInstance' object from the supplied address.
+    /// If the object is not yet loaded, launches an asynchronous loading job and returns a placeholder.
+    /// </summary>
+    /// <param name="name">Name of the GameObject (visible from the Editor in Play mode).</param>
+    /// <param name="address">Address of the resource to load.</param>
+    /// <param name="coordinateFrame">Coordinate frame of the object. See Keyframe.cs.</param>
+    /// <returns>Object instance.</returns>
+    public static GfxReplayInstance LoadAndInstantiate(string name, string address, Frame coordinateFrame)
+    {
+        var newInstance = new GameObject(name).AddComponent<GfxReplayInstance>();
+        newInstance.Load(address, coordinateFrame);
+        return newInstance;
+    }
+
+    /// <summary>
+    /// Destroy the GameObject as well as all of its children.
+    /// </summary>
+    public void Destroy()
+    {
+        // OnDisable() will be called by the engine after this.
+        Destroy(this.gameObject);
+    }
+
+    // TODO: Optimization: Skip the offset node. Instead, bake the transform into the instance root node.
+    GameObject CreateOffsetNode(Frame frame)
+    {
+        GameObject offsetNode = new GameObject("Offset");
+        offsetNode.transform.localRotation = CoordinateSystem.ComputeFrameRotationOffset(frame);
+        return offsetNode;
+    }
+
+
+    void Load(string address, Frame frame)
+    {
+        // TODO: Asynchronous resource loading.
+        GameObject prefab = Resources.Load<GameObject>(address);
+
+        if (prefab == null)
+        {
+            Debug.LogError($"Unable to load GameObject for '{address}'.");
+            return;
+        }
+
+        GameObject offsetNode = CreateOffsetNode(frame);
+        GameObject instance = Instantiate(prefab);
+        offsetNode.transform.SetParent(transform, worldPositionStays: false);
+        instance.transform.SetParent(offsetNode.transform, worldPositionStays: false);
+    }
+
+    // MonoBehaviour function that is called when the object is destroyed.
+    private void OnDisable()
+    {
+        // TODO: Stop asynchronous loading.
+    }
+}
